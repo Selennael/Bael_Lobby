023721b81cb7d5b7611830b6053fcdd4bff63a9d
Selennael <selennael.klein@gmail.com>
2025-04-06 20:31:54 +0700

    Delete pages directory

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: 'center', padding: '2rem' }}>
-      <h1>Welcome to Bael's Site</h1>
-      <p>This is my personal portfolio for cosplay and YouTube content.</p>
-      {/* Add sections for your video thumbnails, social links, etc. */}
-    </div>
-  );
-}
